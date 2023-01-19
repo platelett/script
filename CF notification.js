b23ccc1ba7f814f1da0a76d5958d4478336cd578
platelett <79297685+platelett@users.noreply.github.com>
2023-01-19 23:59:40 +0800

    Update to 0.2.1
    新特性：
    - "Happy New Year!", "Pretests passed" 和 "Perfect result" 归类为 AC。
    - 通知界面布局优化，显示难度（如果有的话）。
    - 点击通知可以打开题目。
    修复漏洞：
    - 之前会遗漏 "In queue" 但后来 AC 的提交。
    - 提交记录只能翻 5 页，如果关注了非常多的人会导致一些未读提交溢出到 5 页之外，之前程序会死循环，现在会忽略这些提交。

--- CF notification.js
+++ CF notification.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         CF notification
 // @namespace    https://github.com/platelett/script
-// @version      0.1
-// @description  Get notified when your friend solves a problem on codeforces. 想知道你的同学有多卷吗？
+// @version      0.2.1
+// @description  Get notified when your friend solves a problem on codeforces. 看看是谁在卷题
 // @author       platelet
 // @match        https://codeforces.com/problemset/status?friends=on
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=codeforces.com
@@ -16,32 +16,65 @@
 (function() {
     'use strict';
     onload = () => {
-        const interval = 1000 * 120;
+        const interval = 1000 * 12;
+        var parser = new DOMParser();
+        const query = async (url, path) => parser.parseFromString(await (await fetch(url)).text(), "text/html").querySelectorAll(path);
         Notification.requestPermission(() => {
             if(document.querySelector("#header > div.lang-chooser > div:nth-child(2) > a:nth-child(2)").textContent == "Register") {
                 alert("You are not logged in and cannot receive notifications.");
                 return;
             }
-            var parser = new DOMParser();
             var last = document.querySelector("#pageContent > div.datatable > div:nth-child(6) > table > tbody > tr > td.id-cell.dark.left > a").textContent;
             var running = new Set();
             var ID = setInterval(async () => {
                 try {
-                    var now;
-                    for(var page = 1;; page++) {
-                        var list = parser.parseFromString(await (await fetch("https://codeforces.com/problemset/status/page/" + page + "?friends=on")).text(), "text/html").querySelectorAll("#pageContent > div.datatable > div:nth-child(6) > table > tbody > tr:not(.first-row)");
-                        if(!list.length) throw "Invalid page";
-                        if(page == 1) now = list[0].children[0].textContent.trim();
-                        for(var submission of list) {
-                            var info = [];
-                            for(var i of submission.children) if(i.children[0]) info.push(i.children[0].textContent.trim());
-                            if(info[0] <= last && (!running.size || info[0] < Math.min.apply(null, [...running]))) { last = now; return; }
-                            var visited = info[0] <= last;
-                            if(running.has(info[0])) running.delete(info[0]), visited = false;
-                            if(visited) continue;
-                            if(info[4] == "Accepted") new Notification(info[2] + " has solved " + info[3] + ".");
-                            if(info[4].startsWith("Running on test")) running.add(info[0]);
+                    var now, result = [];
+                    await (async () => {
+                        for(var page = 1; page <= 5; page++) {
+                            var list = await query("https://codeforces.com/problemset/status/page/" + page + "?friends=on",
+                                "#pageContent > div.datatable > div:nth-child(6) > table > tbody > tr:not(.first-row)");
+                            if(!list.length) throw "Invalid page";
+                            if(page == 1) now = list[0].children[0].textContent.trim();
+                            for(var submission of list) {
+                                var info = [];
+                                for(var i of submission.children) {
+                                    if(i.children[0]) i = i.children[0];
+                                    info.push(i.textContent.trim());
+                                }
+                                if(info[0] <= last && (!running.size || info[0] < Math.min.apply(null, [...running]))) return;
+                                var visited = info[0] <= last;
+                                if(running.has(info[0])) running.delete(info[0]), visited = false;
+                                if(visited) continue;
+                                if(info[5] == "Accepted" || info[5] == "Happy New Year!" ||
+                                info[5].startsWith("Pretests passed") || info[5].startsWith("Perfect result")) {
+                                    var parts = info[3].split(" ");
+                                    result.push({
+                                        title: info[2] + " has solved " + parts[0],
+                                        user: info[2],
+                                        problem: submission.children[3].children[0].href,
+                                        name: parts[0].slice(parts[0].match(/[A-Z]/).index) + ". " + parts.slice(2).join("")
+                                    });
+                                }
+                                if(info[5] == "In queue" || info[5].startsWith("Running on test")) running.add(info[0]);
+                            }
                         }
+                        running.clear();
+                    })();
+                    last = now;
+                    var all = [];
+                    for(var i of result) all.push(new Promise(async resolve => {
+                        var tmp = i;
+                        for(var node of await query(tmp.problem, "span.tag-box")) {
+                            var tag = node.textContent.trim();
+                            if(tag[0] == '*') tmp.title += " (" + parseInt(tag.slice(1)) + ")";
+                        }
+                        tmp.title += ".", resolve();
+                    }));
+                    await Promise.all(all);
+                    while(result.length) {
+                        var i = result.pop();
+                        if(!i.duplicate) new Notification(i.title, { body: i.name }).onclick
+                            = () => window.open(i.problem, '_blank');
                     }
                 } catch(err) {
                     new Notification("Error happened! Please check the console.");
